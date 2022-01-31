8b88ffbd2db7574b7bf9aef9a23b9f7f2826f8fa
LUCIANO ARAUJO MENESES - GETIC - GETIC <214083@int.cagece.com.br>
2022-01-31 13:06:25 -0300

    Refatoração do Pageable

--- src/main/java/com/devsuperior/desafioCrud/resources/ClientResource.java
+++ src/main/java/com/devsuperior/desafioCrud/resources/ClientResource.java
@@ -1,9 +1,9 @@
 package com.devsuperior.desafioCrud.resources;
+
 import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.devsuperior.desafioCrud.dto.ClientDTO;
@@ -26,13 +25,8 @@ public class ClientResource {
 	private ClientService service;
 
 	@GetMapping
-	public ResponseEntity<Page<ClientDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
-
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		Page<ClientDTO> list = service.findAllPaged(pageRequest);
+	public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+		Page<ClientDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
 	}
 
@@ -47,7 +41,6 @@ public class ClientResource {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
-
 	}
 
 	@PutMapping(value = "/{id}")
@@ -60,6 +53,5 @@ public class ClientResource {
 	public ResponseEntity<ClientDTO> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
-
 	}
 }
